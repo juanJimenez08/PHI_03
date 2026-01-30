8c1d70b714e26046e8219fe116780efb72a8d3b2
juanJimenez08 <juan.jimenez@itec.ufpa.br>
2026-01-30 14:46:41 -0300

    modificações gerais

--- bloco_operacional.vhd
+++ bloco_operacional.vhd
@@ -21,22 +21,63 @@ architecture rtl of bloco_operacional is
     type ram_type is array (0 to 31) of std_logic_vector(7 downto 0);
     signal ram : ram_type := (others => x"20"); -- Espaços
 
-    function char_to_seg(char : std_logic_vector(7 downto 0)) return std_logic_vector is
-        variable hex_val : integer;
+   function char_to_seg(char : std_logic_vector(7 downto 0)) return std_logic_vector is
         variable seg : std_logic_vector(6 downto 0);
+        variable char_int : integer;
     begin
-        -- (Mesma função de decodificação anterior - resumida aqui)
-        if (unsigned(char) >= 48 and unsigned(char) <= 57) then hex_val := to_integer(unsigned(char)) - 48;
-        elsif (unsigned(char) >= 65 and unsigned(char) <= 70) then hex_val := to_integer(unsigned(char)) - 55;
-        else hex_val := 16; end if;
+        char_int := to_integer(unsigned(char));
         
-        case hex_val is
-            when 0 => seg := "1000000"; when 1 => seg := "1111001"; when 2 => seg := "0100100";
-            when 3 => seg := "0110000"; when 4 => seg := "0011001"; when 5 => seg := "0010010";
-            when 6 => seg := "0000010"; when 7 => seg := "1111000"; when 8 => seg := "0000000";
-            when 9 => seg := "0010000"; when 10=> seg := "0001000"; when 11=> seg := "0000011";
-            when 12=> seg := "1000110"; when 13=> seg := "0100001"; when 14=> seg := "0000110";
-            when 15=> seg := "0001110"; when others => seg := "1111111";
+        -- Lógica: 0 é ligado, 1 é desligado (para display Anodo Comum/Padrão DE10/DE2)
+        -- Segmentos: gfedcba
+        case char_int is
+            -- Números (0-9)
+            when 48 => seg := "1000000"; -- 0
+            when 49 => seg := "1111001"; -- 1
+            when 50 => seg := "0100100"; -- 2
+            when 51 => seg := "0110000"; -- 3
+            when 52 => seg := "0011001"; -- 4
+            when 53 => seg := "0010010"; -- 5
+            when 54 => seg := "0000010"; -- 6
+            when 55 => seg := "1111000"; -- 7
+            when 56 => seg := "0000000"; -- 8
+            when 57 => seg := "0010000"; -- 9
+            
+            -- Letras Maiúsculas (A-Z)
+            when 65 => seg := "0001000"; -- A
+            when 66 => seg := "0000011"; -- B (b minúsculo)
+            when 67 => seg := "1000110"; -- C
+            when 68 => seg := "0100001"; -- D (d minúsculo)
+            when 69 => seg := "0000110"; -- E
+            when 70 => seg := "0001110"; -- F
+            when 71 => seg := "0000010"; -- G (Igual ao 6)
+            when 72 => seg := "0001001"; -- H
+            when 73 => seg := "1111001"; -- I (Igual ao 1)
+            when 74 => seg := "1100001"; -- J
+            when 75 => seg := "0001001"; -- K (Igual ao H)
+            when 76 => seg := "1000111"; -- L
+            when 77 => seg := "0001000"; -- M (Não dá pra fazer bem, parece A)
+            when 78 => seg := "0101011"; -- N (n minúsculo)
+            when 79 => seg := "1000000"; -- O (Igual ao 0)
+            when 80 => seg := "0001100"; -- P
+            when 81 => seg := "0011000"; -- Q (parece um 9 espelhado/g)
+            when 82 => seg := "0101111"; -- R (r minúsculo)
+            when 83 => seg := "0010010"; -- S (Igual ao 5)
+            when 84 => seg := "0000111"; -- T (t minúsculo)
+            when 85 => seg := "1000001"; -- U
+            when 86 => seg := "1000001"; -- V (Igual ao U)
+            when 87 => seg := "1000001"; -- W (Não dá pra fazer, usa U)
+            when 88 => seg := "0001001"; -- X (Igual ao H)
+            when 89 => seg := "0010001"; -- Y
+            when 90 => seg := "0100100"; -- Z (Igual ao 2)
+
+            -- Letras Minúsculas (a-z) - Mapeia para as mesmas das maiúsculas ou ajusta
+            when 97 to 122 => 
+                -- Chamada recursiva ou copia a lógica acima se precisar
+                seg := "1111111"; -- Simplificando: Desligado para minúsculas não tratadas
+
+            -- Espaço e outros
+            when 32 => seg := "1111111"; -- Espaço (Space)
+            when others => seg := "1111111"; -- Apagado
         end case;
         return seg;
     end function;
---
+++ software/display/obj/default/hello_world_small.d
@@ -0,0 +1,9 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ c:/Users/juane/OneDrive/Documentos/QUARTUS/PROJETO_3_PHI/software/display_bsp/system.h \
+ c:/Users/juane/OneDrive/Documentos/QUARTUS/PROJETO_3_PHI/software/display_bsp/linker.h \
+ c:\users\juane\onedrive\documentos\quartus\projeto_3_phi\software\display_bsp\hal\inc\io.h \
+ c:\users\juane\onedrive\documentos\quartus\projeto_3_phi\software\display_bsp\hal\inc\alt_types.h
+c:/Users/juane/OneDrive/Documentos/QUARTUS/PROJETO_3_PHI/software/display_bsp/system.h:
+c:/Users/juane/OneDrive/Documentos/QUARTUS/PROJETO_3_PHI/software/display_bsp/linker.h:
+c:\users\juane\onedrive\documentos\quartus\projeto_3_phi\software\display_bsp\hal\inc\io.h:
+c:\users\juane\onedrive\documentos\quartus\projeto_3_phi\software\display_bsp\hal\inc\alt_types.h:
